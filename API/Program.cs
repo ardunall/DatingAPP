46f2856afd74f6a5c388a9c468fbb1acfbb85265
arda123 <arda.unal@live.acibadem.edu.tr>
2025-08-24 18:36:11 +0300

    api skeleton

---
+++ API/Program.cs
@@ -0,0 +1,35 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+
+}
+
+app.MapControllers();
+
+app.Run();
